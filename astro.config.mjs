f4711d35d9ae2a763343b0d74e7d112d3ca14050
岳士超 <custom_code@outlook.com>
2026-06-07 17:24:41 +0800

    add: 新增vue支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		vue(),
 		starlight({
 			title: 'Buct Wiki',
 			components: {
